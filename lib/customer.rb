4c4bca4ba1c03c582395aedf441e951be964c059
Doug Jones <dsjr1284@gmail.com>
2020-04-19 19:49:48 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = "25"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(Waiter, self, total, tip)
+  end
+
+  # def new_meal(customer, total, tips)
+  #   Meal.new(self, customer, total, tips)
+  # end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    meals.map {|meal| meal.waiter}
+  end
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
+  attr_accessor :name, :years_of_experience
+
+  @@all = []
+
+  def initialize(name, years_of_experience)
+    @name = name
+    @yrs_experience = years_of_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tips)
+    Meal.new(self, customer, total, tips)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max { |meal_1, meal_2| meal_1.tip <=> meal_2.tip}
+    best_tipped_meal.customer
+  end
+
+
 
 end
